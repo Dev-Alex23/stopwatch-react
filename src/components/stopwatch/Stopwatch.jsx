fc751a49378e783ff6654edf58857ec08f55165a
Dev-Alex23 <alexhinds1@outlook.com>
2022-10-09 17:21:29 +0100

    feat - Current timer display & button controlls

--- src/components/stopwatch/Stopwatch.jsx
+++ src/components/stopwatch/Stopwatch.jsx
@@ -4,48 +4,120 @@ import "./Stopwatch.css";
 
 const Stopwatch = () => {
   const [isTimerRunning, setIsTimerRunning] = useState(false);
-  const [time, setTime] = useState(0);
+  const [elasptime, setElasptime] = useState(0);
+  const [laps, setLaps] = useState([]);
+
   let interval;
+  const lapData = {
+    lap: [...laps],
+  };
 
   useEffect(() => {
     if (isTimerRunning) {
-      let startTime = Date.now() - time;
-      console.log(startTime);
+      let startTime = Date.now() - elasptime;
       interval = setInterval(() => {
-        setTime(Date.now() - startTime);
-        console.log(getFormattedTime(startTime));
+        setElasptime(Date.now() - startTime);
       }, 10);
     }
 
     return () => clearInterval(interval);
   }, [isTimerRunning]);
 
-  const startStopButton = isTimerRunning ? "Stop" : "Start";
-  const lapResetButton = isTimerRunning ? "Lap" : "Reset";
-  const startStopClass = isTimerRunning
-    ? "primary-button stop-button"
-    : "primary-button start-button";
+  useEffect(() => {
+    if (elasptime > 0) {
+      const currentLap = lapData.lap[0] ?? { lapNumber: 1 };
+    }
+  }, [elasptime]);
 
-  const lapResetClass = isTimerRunning
-    ? "primary-button lap-button"
-    : "primary-button reset-button";
+  const reset = () => {
+    setElasptime(0);
+    setIsTimerRunning(false);
+    setLaps([]);
+  };
 
-  const buttonAction = () => {
-    setIsTimerRunning(!isTimerRunning);
+  const startStopwatch = () => {
+    setIsTimerRunning(true);
+  };
+
+  const insertLaps = () => {
+    let newLap = laps.reduce((prevValue, currentValue) => {
+      return prevValue + currentValue;
+    }, 0);
+    setLaps((prevLaps) => {
+      return [...prevLaps, elasptime - newLap];
+    });
+  };
+
+  const toggleTimer = () => setIsTimerRunning(!isTimerRunning);
+  const lapDisable = !isTimerRunning && elasptime === 0;
+  const startStopButtonText = isTimerRunning ? "Stop" : "Start";
+  const lapResetButtonText = !isTimerRunning && elasptime > 0 ? "Reset" : "Lap";
+  console.log(lapDisable);
+
+  const renderButtons = () => {
+    switch (isTimerRunning) {
+      case false:
+        return (
+          <>
+            <button className='primary-button lap-button' onClick={reset}>
+              Reset
+            </button>
+            <button className='primary-button start-button' onClick={startStopwatch}>
+              Start
+            </button>
+          </>
+        );
+
+      case true:
+        return (
+          <>
+            <button className='primary-button lap-button' onClick={insertLaps}>
+              Lap
+            </button>
+            <button
+              className='primary-button stop-button'
+              onClick={() => setIsTimerRunning(false)}
+            >
+              Stop
+            </button>
+          </>
+        );
+    }
   };
 
   return (
-    <section className="stopwatch">
-      <div className="stopwatch__content">
-        <div className="stopwatch__content timer--container">
-          <p className="time-display">{getFormattedTime(time)}</p>
+    <section className='stopwatch'>
+      <div className='stopwatch__content'>
+        <div className='stopwatch__content timer--container'>
+          <p className='time-display'>{getFormattedTime(elasptime)}</p>
         </div>
-        <div className="stopwatch__content controller-container">
-          <button className={lapResetClass}>{lapResetButton}</button>
-          <button className={startStopClass} onClick={buttonAction}>
-            {startStopButton}
-          </button>
+        <div className='stopwatch__content controller-container'>
+          {
+            <button className='primary-button' disabled={lapDisable}>
+              {lapResetButtonText}
+            </button>
+          }
+          {
+            <button className='primary-button' onClick={toggleTimer}>
+              {" "}
+              {startStopButtonText}
+            </button>
+          }
         </div>
+      </div>
+      <div className='stopwatch__content lap-record__container'>
+        <table>
+          <tbody>
+            {laps?.map((lap, index) => {
+              return (
+                <tr key={index}>
+                  <td>Lap {index + 1}</td>
+                  <td> {getFormattedTime(lap)} </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </section>
   );
--- src/components/utils.js
+++ src/components/utils.js
@@ -4,18 +4,15 @@ let minutes = 0;
 let hours = 0;
 
 const padTo2Digits = (num) => {
-  return num.toString().padStart(2, "0");
+  return num.toString().padStart(2, "0").substring(0, 2);
 };
 
 // convertTime function
 export const getFormattedTime = (duration) => {
-  milliseconds = duration % 100;
-  seconds = Math.floor(duration / 100);
+  milliseconds = duration % 1000;
+  seconds = Math.floor(duration / 1000);
   minutes = Math.floor(seconds / 60);
   hours = Math.floor(minutes / 60);
-
-  seconds = seconds % 60;
-  minutes = minutes % 60;
 
   if (minutes < 60) {
     return `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}.${padTo2Digits(
