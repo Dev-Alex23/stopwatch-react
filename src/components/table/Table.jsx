188fe711607cea48f7d2b7057ecb5eaade3593e6
Alex Hinds <alex.hinds1@outlook.com>
2022-10-13 17:03:14 +0100

    refactor - change from nested if statements to switch block

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -3,22 +3,24 @@ import "./Table.css";
 import { getFormattedTime } from "../utils";
 
 const Table = ({ lapData, isTimerRunning, elapsedTime }) => {
+  const checkMinMaxLap = (val) => {
+    switch (lapData.laps.length >= 2) {
+      case lapData.minLap === val:
+        return "min-lap";
+      case lapData.maxLap === val:
+        return "max-lap";
+      default:
+        return "";
+    }
+  };
+
   return (
     <div className="stopwatch__content lap-record__container">
       <table>
         <tbody>
           {lapData.laps?.map((lap, i) => {
             return (
-              <tr
-                key={`Lap-${i}-${lap}`}
-                className={
-                  lapData.laps.length >= 2
-                    ? `${lapData.minLap === lap && "min-lap"} ${
-                        lapData.maxLap === lap && "max-lap"
-                      }`
-                    : ""
-                }
-              >
+              <tr key={`Lap-${i}-${lap}`} className={checkMinMaxLap(lap)}>
                 <td>Lap {i + 1} </td>
                 <td>{getFormattedTime(lap)}</td>
               </tr>
