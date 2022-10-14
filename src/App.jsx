5d0e07957d501977b05443316616d67efc257712
Alex Hinds <alex.hinds1@outlook.com>
2022-10-14 09:52:10 +0100

    refactor - useReducer to manage state

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,20 @@ import "./App.css";
 function App() {
   const [state, dispatch] = useReducer(INITIAL_STATE, stopwatchReducer);
 
-  const intialData = {
-    laps: [],
-    totalLapTime: 0,
-    runningTime: 0,
-    minLap: Infinity,
-    maxLap: 0,
-  };
-
-  const [isTimerRunning, setIsTimerRunning] = useState(false);
-  const [lapData, setLapData] = useState(intialData);
-  const [elapsedTime, setElapsedTime] = useState(0);
-
   useEffect(() => {
-    let interval;
-    if (isTimerRunning) {
-      let startTime = Date.now() - elapsedTime;
-      interval = setInterval(() => {
-        setElapsedTime(Date.now() - startTime);
+    if (state.isTimerRunning) {
+      const startTime = Date.now - state.elaspedTime;
+      const interval = setInterval(() => {
+        const elapsedTime = Date.now - startTime;
+        dispatch({ type: "START_TIMER", elapsedTime });
       }, 10);
+
+      return () => clearInterval(interval);
     }
     if (elapsedTime > 0) {
-      setLapData((prevLapData) => ({
-        ...prevLapData,
-        runningTime: elapsedTime - lapData.totalLapTime,
-      }));
+      dispatch({ type: "UPDATE_RUNNING_TIME" });
     }
-
-    return () => clearInterval(interval);
-  }, [isTimerRunning, elapsedTime]);
+  }, [state.isTimerRunning, state.elapsedTime]);
 
   const addLaps = () => {
     const currentLapTime = elapsedTime - lapData.totalLapTime;
@@ -56,23 +41,21 @@ function App() {
   };
 
   const resetLaps = () => {
-    setElapsedTime(0);
-    setIsTimerRunning(false);
-    setLapData(intialData);
+    dispatch({ type: "RESET_LAP" });
   };
 
-  const toggleTimer = () => setIsTimerRunning(!isTimerRunning);
-  const addLapRestLaps = isTimerRunning ? addLaps : resetLaps;
+  const toggleTimer = () => dispatch({ type: "TOGGLE_TIMER" });
+  const addLapRestLaps = state.isTimerRunning ? addLaps : resetLaps;
 
-  const isLapDisabled = !isTimerRunning && elapsedTime === 0;
-  const startStopButtonText = isTimerRunning ? "Stop" : "Start";
+  const isLapDisabled = !state.isTimerRunning && state.elapsedTime === 0;
+  const startStopButtonText = state.isTimerRunning ? "Stop" : "Start";
   const lapResetButtonText =
-    !isTimerRunning && elapsedTime > 0 ? "Reset" : "Lap";
-  const startStopClass = isTimerRunning
+    !state.isTimerRunning && state.elapsedTime > 0 ? "Reset" : "Lap";
+  const startStopClass = state.isTimerRunning
     ? "primary-button stop-button"
     : "primary-button start-button";
   const lapResetClass =
-    isTimerRunning || elapsedTime === 0
+    state.isTimerRunning || state.elapsedTime === 0
       ? "primary-button lap-button"
       : "primary-button reset-button";
 
@@ -80,7 +63,7 @@ function App() {
     <section className="stopwatch">
       <div className="stopwatch__content">
         <div className="stopwatch__content timer--container">
-          <p className="time-display">{getFormattedTime(elapsedTime)}</p>
+          <p className="time-display">{getFormattedTime(state.elapsedTime)}</p>
         </div>
         <LapButtons
           toggleTimer={toggleTimer}
@@ -93,7 +76,7 @@ function App() {
         />
       </div>
       <Table
-        lapData={lapData}
+        lapData={state}
         isTimerRunning={isTimerRunning}
         elapsedTime={elapsedTime}
       />
--- src/components/lapReducer.js
+++ src/components/lapReducer.js
@@ -1,3 +1,43 @@
-const INITIAL_STATE = {
-  currentTime: 0,
+export const INITIAL_STATE = {
+  laps: [],
+  intervalID: 0,
+  elaspedTime: 0,
+  startTime: 0,
+  isTimerRunning: false,
+  totalLapTime: 0,
+  runningTime: 0,
+  minLap: Infinity,
+  maxLap: 0,
+};
+
+export const stopwatchReducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_TIMER":
+      return {
+        ...state,
+        isTimerRunning: !state.isTimerRunning,
+      };
+
+    case "RESET_LAP":
+      return INITIAL_STATE;
+
+    case "ADD_LAP": {
+      const currentLapTime = state.elapsedTime - state.totalLapTime;
+      return {
+        ...state,
+        laps: [...state.laps, currentLapTime],
+        totalLapTime: state.elaspedTime,
+        minLap: currentLapTime < state.minLap ? currentLapTime : state.minLap,
+        maxLap: currentLapTime > state.maxLap ? currentLapTime : state.maxLap,
+      };
+    }
+    case "UPDATE_RUNNING_TIME":
+      return {
+        ...state,
+        runningTime: state.elaspedTime - state.totalLapTime,
+      };
+
+    default:
+      throw new Error("Invalid action type: " + action.type);
+  }
 };
